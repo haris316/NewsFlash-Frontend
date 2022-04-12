53d4b57981c6741e6ccca79ca78b0a5526f61c9b
hudailyas <huda.milyas@gmail.com>
2022-04-12 05:31:12 +0500

    notifications ui started

--- App.js
+++ App.js
@@ -53,7 +53,7 @@ export default function App() {
     signIn: (token) => {
       // setUserToken('jdjd');
       // setIsLoading(false);
-      console.log("thisistokr",token);
+      
       setToken(token);
       dispatch({type : 'LOGIN', token : token});
     },
@@ -101,91 +101,3 @@ export default function App() {
 
 
 
-// import { NavigationContainer} from '@react-navigation/native';
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors
-// } from 'react-native/Libraries/NewAppScreen';
-// import MyTabs from "./MyApp";
-
-// const Section = ({children, title}) => {
-//   const isDarkMode = false;
-//   return (
-//     <View style={styles.sectionContainer,{
-//       backgroundColor: isDarkMode ? "#999999" : "#FFFFFF",
-//     }}>
-//       <Text
-//         style={[
-//           styles.sectionTitle,
-//           {
-//             color: isDarkMode ? "#FFFFFF" : "#000000",
-//           },
-//         ]}>
-//         {title}
-//       </Text>
-//       <Text
-//         style={[
-//           styles.sectionDescription,
-//           {
-//             color: isDarkMode ? "#FFFFFF" : "#222222",
-//           },
-//         ]}>
-//         {children}
-//       </Text>
-//     </View>
-//   );
-// };
-
-// const App = () => {
-//   const isDarkMode = false;
-
-//   const backgroundStyle = {
-//     backgroundColor: isDarkMode ? "#222222" : "#FFFFFF",
-//   };
-
-//   return (
-//     <SafeAreaView style={backgroundStyle}>
-//       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-//       <ScrollView
-//         contentInsetAdjustmentBehavior="automatic"
-//         style={backgroundStyle}>
-//         <View
-//           style={{
-//             backgroundColor: isDarkMode ? "#000000" : "#FFFFFF",
-//           }}>
-//             <Section children="This is a text" title="Heading" />
-//           </View>
-//       </ScrollView>
-    
-//     </SafeAreaView>
-  
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   }
-// });
-
-// export default App;
--- components/Notifications/Notifications.js
+++ components/Notifications/Notifications.js
@@ -1,17 +1,128 @@
+
+import { Center } from "native-base";
 import React from "react";
-import { Text, View, Alert } from "react-native";
+import { 
+  Text, 
+  View, 
+  Alert, 
+  SafeAreaView, 
+  ScrollView, 
+  StyleSheet, 
+  TouchableOpacity,
+  Dimensions } 
+  from "react-native";
+  import Ellipsis from 'react-native-vector-icons/Ionicons';
+  import Following from 'react-native-vector-icons/FontAwesome5'
+
+  const {width, height} = Dimensions.get('window');
 
 export default function Notifications() {
         return (
-          <View
-            style={{
-              flex: 1,
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            <Text>Notifssss</Text>
-          </View>
+         <SafeAreaView style = {styles.container}>
+           <View style = {styles.top}>
+           <TouchableOpacity>
+              <Ellipsis name="ellipsis-vertical" size={30}/>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <Ellipsis name="ellipsis-vertical" size={30}/>
+            </TouchableOpacity>
+           </View>
+           <ScrollView>
+             <View style={styles.day}>
+               <Text style = {{fontWeight: '700', fontSize: 25, color:'black'}}>T O D A Y</Text>
+             </View>
+             <View style = {styles.notif}>
+               <View style = {styles.notifContainer}>
+               <View style= {styles.notifIcon}>
+                  <Following name="user-check" size={30} color="#045c5a"/>
+               </View>
+               <View style = {styles.notifText}>
+                 <View style = {styles.time}>
+
+                 </View>
+                 <View style = {styles.text}>
+                   <Text style = {{fontWeight: "bold",fontSize:15}}>
+                     Huda Ilyas
+                   </Text>
+                   <Text style = {{fontSize:15}}>
+                     has started following you
+                   </Text>
+
+                 </View>
+               </View>
+             </View>
+             </View>
+           </ScrollView>
+         </SafeAreaView>
         )
 }
+
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    backgroundColor: '#fff',
+    fontFamily: 'Alegreya Sans',
+  },
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    top: 10
+  },
+
+  day : {
+    marginTop: height/100 *5,
+    justifyContent: 'center',
+    alignItems: "center",
+    marginBottom : height/100 * 2.5
+
+  },
+  notif : {
+    width : width,
+    height: height/100 *20,
+    borderTopWidth: 0.5,
+    borderBottomWidth: 0.5,
+    justifyContent: "center"
+
+    
+  },
+  notifContainer : {
+    width : width/100 *80,
+    borderWidth: 1,
+    top : 0,
+    bottom : 0,
+    position: "absolute",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around"
+    
+   
+  },
+
+  notifIcon :{
+    
+    
+   
+  },
+
+  notifText : {
+    
+    borderWidth: 1,
+    
+    
+  },
+
+  time : {
+
+  },
+  text : {
+    width : (width/100 *80) /100 * 50,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    
+   
+  }
+
+
+})
 
